2e55843e5529997da3a1ecf65f22f59f9f5648ff
tomomiik <tomomiik0210@gmail.com>
2016-12-29 04:18:31 +0000

    secondcommit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks　
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
